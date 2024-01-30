4d02c53ae6070ed5a052f434083ead8be5f7cc3f
bassyu <yrkdgus@gmail.com>
2024-01-31 02:05:21 +0900

    프로젝트 초기 세팅
    - 프리티어 설정
    - 필요없는 파일 삭제

---
+++ app/app.css.ts
@@ -0,0 +1,11 @@
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  width: '100%',
+  maxWidth: 1200,
+  height: '100%',
+});
+
+export const header = style({
+  width: '100%',
+});
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
+const widthVanillaExtract = createVanillaExtractPlugin();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default widthVanillaExtract(nextConfig);
