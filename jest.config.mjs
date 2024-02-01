e7c72f9e8ed0bbb38a7e216c389cb509d34a0830
maaaaaaaaad <integral5137@gmail.com>
2024-02-01 21:19:11 +0900

    setup to jest the first test code

---
+++ jest.config.mjs
@@ -0,0 +1,11 @@
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+const config = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  preset: 'ts-jest',
+};
+export default createJestConfig(config);
