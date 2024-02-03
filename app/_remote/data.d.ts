c4aaf97bec37967ab910f5d1ae5ab22d828a4e9e
bassyu <yrkdgus@gmail.com>
2024-02-03 15:22:09 +0900

    모의 데이터 추가

---
+++ app/_remote/data.d.ts
@@ -0,0 +1,20 @@
+interface Writing {
+  id: string;
+  owner: {
+    nickname: string;
+    imageUrl: string;
+  };
+  content: string;
+  likes: number;
+  isVideo: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface WritingList {
+  data: Writing[];
+  size: number;
+  take: number;
+  totalPage: number;
+  totalCount: number;
+}
