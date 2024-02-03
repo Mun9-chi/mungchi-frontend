e77e552146a0ab43022893847dbf2ba341b1d5ff
bassyu <yrkdgus@gmail.com>
2024-02-03 15:23:09 +0900

    홈에서 임의 writings 데이터로 masonry 카드 렌더링

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,33 @@
 import Image from 'next/image';
 import * as style from './app.css';
+import Masonry from './_components/Masonry/Masonry';
+
+import mockWritings from './_remote/mock-writings.json';
+import MasonryItem from './_components/Masonry/MasonryItem';
+import Card from './_components/Card/Card';
 
 export default function Home() {
+  const writings = mockWritings;
+
   return (
     <>
       <header className={style.header}>
         <div className={style.headerContainer}>
-          <div>MUNGCHI</div>
+          <span>MUNGCHI</span>
           <Image src="/icons/search.svg" alt="검색" width={12} height={12} />
         </div>
       </header>
-      <main></main>
+      <main className={style.container}>
+        <Masonry>
+          {writings.map((writing) => {
+            return (
+              <MasonryItem key={writing.id}>
+                <Card {...writing} />
+              </MasonryItem>
+            );
+          })}
+        </Masonry>
+      </main>
     </>
   );
 }
