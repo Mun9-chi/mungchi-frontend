9d3e14204182728a361c5fa998e9a3dc093291e7
bassyu <yrkdgus@gmail.com>
2024-02-03 15:19:58 +0900

    임의 이미지 데이터를 위한 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,23 @@ import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 const widthVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/*',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '/*',
+      },
+    ],
+  },
+};
 
 export default widthVanillaExtract(nextConfig);
