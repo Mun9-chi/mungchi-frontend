c7560e912409a7aa2209493314c1ed4e5b097009
유강현 <yrkdgus@gmail.com>
2024-03-05 19:36:59 +0900

    대문자 파일 삭제

--- app/_components/Masonry/index.ts
+++
@@ -1,6 +0,0 @@
-import MasonryContainer from './masonry-container';
-import MasonryItem from './masonry-item';
-
-const Masonry = Object.assign(MasonryContainer, { Item: MasonryItem });
-
-export default Masonry;
--- app/_components/Masonry/masonry-container.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import * as style from './masonry.css';
-
-export default function MasonryContainer(props: React.PropsWithChildren) {
-  return <div className={style.masonryContainer}>{props.children}</div>;
-}
